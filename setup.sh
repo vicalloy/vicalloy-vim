3b3362d928bb1a7a2f77223749a53ac10646e997
vicalloy <zbirder@gmail.com>
2012-12-20 16:34:33 +0800

    pip install jedi,pyflakes

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 #rm -rf ~/.vimrc
 #rm -rf ~/.vim/
 sudo apt-get install ttf-dejavu
+sudo pip install pyflakes
+sudo pip install jedi
 cp ./_vimrc ~/.vimrc
 mkdir ~/.vim/
 cp -p -R ./vimfiles/** ~/.vim/
